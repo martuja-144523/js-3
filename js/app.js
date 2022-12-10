9579e3dccdb9eec170738aa21d2057f626273ada
martuja-144523 <116412317+martuja-144523@users.noreply.github.com>
2022-12-10 08:21:50 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('nav-toggler').addEventListener( click , function() {
+    document.getElementById('s-nav').style.display ='block'
+})
+
+$('#nav-toggler').on(click , function() {
+    $('.side-nav').style.display = 'block'
+})
